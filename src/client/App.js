25aeecc1b28e70e855aa8b5562b2ae827a8523a8
Adrián <statickidz@gmail.com>
2019-01-16 01:07:48 +0100

    Added site

---
+++ src/client/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import './app.scss'
+import ReactPlayer from 'react-player'
+import Carousel from 'nuka-carousel'
+
+export default class App extends Component {
+  state = {
+    feed: [],
+    playing0: true,
+    playing1: false,
+    playing2: false,
+    playing3: false,
+    playing4: false,
+  }
+
+  componentDidMount() {
+    this.loadFeed()
+  }
+
+  loadFeed() {
+    fetch('/api/feed')
+      .then(res => res.json())
+      .then(feed => {
+        console.log(feed)
+        this.setState({ feed: feed.feed })
+      })
+  }
+
+  onVideoEnded = (index) => {
+    if (index === 4) {
+      this.loadFeed()
+    } else {
+      this.refs.slider.nextSlide()
+    }
+  }
+
+  renderSlides = () => {
+    return this.state.feed.map((item, index) => {
+      return (
+        <div className="slide" key={`video-${item.aweme_id}`}>
+          <div className="author">
+            <img className="avatar" src={item.author.avatar_thumb.url_list[0]} />
+            <div>
+              <div className="name">@{item.author.unique_id}</div>
+              <div className="desc">{item.desc}</div>
+            </div>
+          </div>
+          <ReactPlayer
+            className="video"
+            ref={`video${index}`}
+            playing={this.state[`playing${index}`]}
+            url={item.video.play_addr.url_list[0]}
+            onEnded={() => this.onVideoEnded(index)}
+          />
+        </div>
+      )
+    })
+  }
+
+  render() {
+    const settings = {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      vertical: true,
+      dragging: false,
+      withoutControls: true,
+      beforeSlide: (currentSlide, nextSlide) => {
+        this.setState({ [`playing${currentSlide}`]: false })
+      },
+      afterSlide: (currentSlide) => {
+        this.setState({ [`playing${currentSlide}`]: true })
+      }
+    }
+
+    return (
+      <div>
+        <Carousel ref="slider" {...settings}>
+          {this.renderSlides()}
+        </Carousel>
+      </div>
+    )
+  }
+}
---
+++ src/server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const os = require('os');
+//import TikTokAPI, { getRequestParams } from 'tiktok-api';
+const TikTokAPI = require('tiktok-api').default;
+const getRequestParams = require('tiktok-api').getRequestParams;
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/feed', async (req, res) => {
+  // Required - a method that signs the URL with anti-spam parameters
+  // You must provide an implementation yourself to successfully make
+  // most requests with this library.
+  const signURL = async (url, ts, deviceId) => {
+    const as = 'nospam-1'+deviceId;
+    const cp = 'nospam-2'+deviceId;
+    const mas = 'nospam-3'+deviceId;
+    return `${url}&as=${as}&cp=${cp}&mas=${mas}`;
+  }
+
+  // Required - device parameters
+  // You need to source these using a man-in-the-middle proxy such as mitmproxy,
+  // CharlesProxy or PacketCapture (Android)
+  const params = getRequestParams({
+    device_id: '235235235235235',
+    fp: '<device_fingerprint>',
+    iid: '<install_id>',
+    openudid: '<device_open_udid>',
+  });
+
+  const api = new TikTokAPI(params, { signURL });
+
+  await api.listForYouFeed()
+    .then(response => res.send({ feed: response.data.aweme_list }))
+    .catch(console.log);
+});
+
+app.listen(8080, () => console.log('Listening on port 8080!'));
